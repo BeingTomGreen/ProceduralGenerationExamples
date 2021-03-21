2447cac3730eadcad88faca82da31cbc2c6cd854
Tom Green <kingofcuddles@gmail.com>
2021-03-21 04:52:00 +0000

    work on basic procedural gen
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,45 +1,105 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
     [Header("World Config:")]
     public GameObject terrainBlock;
-    public int worldSizeX = 40;
-    public int worldSizeZ = 40;
+    public int blockSize;
+    public int worldSizeX;
+    public int worldSizeZ;
 
     [Header("Height Noise Config:")]
-    public int noiseHeightModifier = 5;
-    public float detailScale = 8f;
+    public int noiseHeightModifier;
+    public float detailScale;
 
-    private List<Vector3> blockPositions = new List<Vector3>();
-    private float gridOffset = 1f;
+    [Header("Player Config:")]
+    public GameObject player;
+    private Vector3 startPosition;
 
-    [Header("Object Config:")]
-    public GameObject objectPrefab;
-    public int objectCount;
-    private float objectSpawnHeightOffset = .5f;
+    private Hashtable blockContainer = new Hashtable();
+
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        for (int x = 0; x < worldSizeX; x++)
+        for (int x = -worldSizeX; x < worldSizeX; x++)
         {
-            for (int z = 0; z < worldSizeZ; z++)
+            for (int z = -worldSizeZ; z < worldSizeZ; z++)
             {
                 // Calulate position of new block
-                Vector3 position = new Vector3(x * gridOffset, generateHeightNoise(x, z) , z * gridOffset);
+                Vector3 position = new Vector3(x * blockSize + startPosition.x, generateHeightNoise(x, z), z * blockSize + startPosition.z);
 
                 // Create new block
-                _ = Instantiate(terrainBlock, position, Quaternion.identity, transform);
+                GameObject block = Instantiate(terrainBlock, position, Quaternion.identity, transform);
 
-                // Add the new blocks position our list
-                blockPositions.Add(position);
+                // Add the new block to our hash table
+                blockContainer.Add(position, block);
             }
         }
-
-        SpawnObjects();
     }
+
+    private void Update()
+    {
+        // Is distance player has moved greater than chunkContainer?
+        if (Mathf.Abs(playerMoveOffsetX) >= blockSize || Mathf.Abs(playerMoveOffsetZ) >= blockSize)
+        {
+            for (int x = -worldSizeX; x < worldSizeX; x++)
+            {
+                for (int z = -worldSizeZ; z < worldSizeZ; z++)
+                {
+                    // Calulate position of new block
+                    Vector3 position = new Vector3(
+                        x * blockSize + playerLocationX,
+                        generateHeightNoise(x + playerLocationX, z + playerLocationZ),
+                        z * blockSize + playerLocationZ
+                    );
+
+                    // Check that block doesn't already exist
+                    if (!blockContainer.ContainsKey(position))
+                    {
+                        // Create new block
+                        GameObject block = Instantiate(terrainBlock, position, Quaternion.identity, transform);
+
+                        // Add the new block to our hash table
+                        blockContainer.Add(position, block);
+                    }
+                }
+            }
+        }
+    }
+
+    private int playerMoveOffsetX
+    {
+        get
+        {
+            return (int)(player.transform.position.x - startPosition.x);
+        }
+    }
+    private int playerMoveOffsetZ
+    {
+        get
+        {
+            return (int)(player.transform.position.z - startPosition.z);
+        }
+    }
+
+    private int playerLocationX
+    {
+        get
+        {
+            return (int)Mathf.Floor(player.transform.position.x);
+        }
+    }
+
+    private int playerLocationZ
+    {
+        get
+        {
+            return (int)Mathf.Floor(player.transform.position.z);
+        }
+    }
+
 
     private float generateHeightNoise(int x, int z)
     {
@@ -49,26 +109,4 @@ public class TerrainGenerator : MonoBehaviour
         return Mathf.PerlinNoise(noiseX, noiseZ) * noiseHeightModifier;
     }
 
-    private void SpawnObjects()
-    {
-        for (int i = 0; i < objectCount; i++)
-        {
-            Vector3 position = GetObjectSpawnLocation();
-            _ = Instantiate(objectPrefab, position, Quaternion.identity, transform);
-        }
-    }
-
-    private Vector3 GetObjectSpawnLocation()
-    {
-        // Grab a random index
-        int rndIndex = Random.Range(0, blockPositions.Count);
-
-        // Generate a new spawn position
-        Vector3 spawnLocation = new Vector3(blockPositions[rndIndex].x, blockPositions[rndIndex].y + objectSpawnHeightOffset, blockPositions[rndIndex].z);
-
-        // Remove this block from the list to prevent dupes
-        blockPositions.RemoveAt(rndIndex);
-
-        return spawnLocation;
-    }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Simple Block Terrain & Object Generation in Unity:
+## Simple Procedural Block Terrain Generation in Unity:
 
-An example using Unity to create simple "randomly" generated terrain, this uses simple prefabs for terrain blocks and items and perlin noise to generate the terrain's height variation. This is almost certainly not how it should be done.
+An example using Unity to create simple "randomly" procedurally generatedterrain, this uses simple prefabs for terrain blocks and items and perlin noise to generate the terrain's height variation. This is almost certainly not how it should be done.
 
 ### Thanks & Sources:
 
